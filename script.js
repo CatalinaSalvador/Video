c9c073d5a07bfdcc1730549c1b54861f78c56e64
MonserratRivas <catalinarivas.iccp@gmail.com>
2023-12-29 04:05:22 -0300

    iniciando repositorio

---
+++ script.js
@@ -0,0 +1,22 @@
+function playvideo(video)
+{
+    video.play()
+}
+
+function stopvideo(video)
+{
+    video.pause()
+}
+
+function turnOff(element) {
+    if (element.innerText == "subscribed") {
+        console.log;
+        element.innerText = "Subscribe";
+    }
+
+    else {
+        element.innerText = "Subscribed"
+        console.log;
+    }
+}
+
